05e8935308c1eb41deaee7a2c57026bc997b8453
rud4038 <mm453@naver.com>
2023-01-06 12:52:01 +0900

    spring boot 공부

--- src/main/java/com/kyungsu/board/service/AuthService.java
+++ src/main/java/com/kyungsu/board/service/AuthService.java
@@ -6,6 +6,7 @@ import com.kyungsu.board.dto.auth.AuthPostDto;
 import com.kyungsu.board.dto.auth.LoginDto;
 import com.kyungsu.board.dto.response.ResponseDto;
 
+// @Service : 해당 클래스가 Service 레이어 역할을 함
 @Service
 public class AuthService {
 
